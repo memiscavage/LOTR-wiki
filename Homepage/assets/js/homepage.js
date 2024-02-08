2da9396a3ed97319d3af634a674845445ec4c45d
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-07 20:22:30 -0600

    cleaned up js

--- Homepage/assets/js/homepage.js
+++ Homepage/assets/js/homepage.js
@@ -1,25 +1,8 @@
-// to-dos up here
-
-// api keys
-// lotr key
+// API Keys
+// LOTR Key
 var lotrAPI = "ql0OUYOIpoNhyZBMrR2O";
 
-//TO DO: Declare variables for document.querySelector to dynamically change text through HTML when 
-//displaying info on content page
-//TO DO: add other variables here
-
-//TO DO: Create click event for search button on homepage
-//TO DO: Create click event for hamburger on mobile homepage
-
-
-
-
-
-
-
-
-
-// api stuffs
+// API Pull
 
 // copy-pasted from 06-Server-Side-APIs > 01-Activities > 05-Ins_Different_APIs > assets > js > script.js
 var requestUrl = 'https://the-one-api.dev/v2/character';
@@ -45,3 +28,15 @@ $.ajax({
         console.log(response);
     }
 })
+
+
+
+
+
+
+//TO DO: Declare variables for document.querySelector to dynamically change text through HTML when 
+//displaying info on content page
+//TO DO: add other variables here
+
+//TO DO: Create click event for search button on homepage
+//TO DO: Create click event for hamburger on mobile homepage
