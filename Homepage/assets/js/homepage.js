2d0ce3c08bbbbd54dab39dc37fd396ff4c19611a
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-07 20:08:18 -0600

    pulling a request together. still not working, need to link jquery.

--- Homepage/assets/js/homepage.js
+++ Homepage/assets/js/homepage.js
@@ -14,13 +14,45 @@ var lotrAPI = "ql0OUYOIpoNhyZBMrR2O";
 
 
 
+
+
+
+
+
 // api stuffs
-var baseLotrURL = "https://the-one-api.dev/v2/";
-var charactersURL = baseLotrURL + "character" + "&appid=" + lotrAPI;
-async function pullLotrAPI() {
-    const lotrResponse = await fetch(charactersURL);
-    const charactersInfo = await lotrResponse.json();
-    console.log(charactersInfo);
-}
-// not working????????????
-pullLotrAPI();
+
+// copy pasted from 06-Server-Side-APIs > 01-Activities > 05-Ins_Different_APIs > assets > js > script.js
+var requestUrl = 'https://the-one-api.dev/v2/character';
+//Browser XMLHttpRequest, built in the browser and require more code.
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        console.log('XMLHttpRequest Response \n-------------');
+        console.log(xhr.response);
+    }
+};
+xhr.open('GET', requestUrl);
+xhr.send();
+
+// copy pasted from stack overflow lol: https://stackoverflow.com/questions/51506579/sending-authorization-token-bearer-through-javascript
+$.ajax({
+    url: requestU,
+    type: 'GET',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer " + lotrAPI);
+    },
+    success: function (response) {
+        console.log(response);
+    }
+})
+
+// old broken
+//var baseLotrURL = "https://the-one-api.dev/v2/";
+//var charactersURL = baseLotrURL + "character" + "&appid=" + lotrAPI;
+//async function pullLotrAPI() {
+//    const lotrResponse = await fetch(charactersURL);
+//    const charactersInfo = await lotrResponse.json();
+//    console.log(charactersInfo);
+//}
+//// not working????????????
+//pullLotrAPI();
