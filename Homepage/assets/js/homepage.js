9281bfb0b1385ced9db8069c8d31f51e00950046
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-07 20:13:58 -0600

    deleted old broken code

--- Homepage/assets/js/homepage.js
+++ Homepage/assets/js/homepage.js
@@ -21,7 +21,7 @@ var lotrAPI = "ql0OUYOIpoNhyZBMrR2O";
 
 // api stuffs
 
-// copy pasted from 06-Server-Side-APIs > 01-Activities > 05-Ins_Different_APIs > assets > js > script.js
+// copy-pasted from 06-Server-Side-APIs > 01-Activities > 05-Ins_Different_APIs > assets > js > script.js
 var requestUrl = 'https://the-one-api.dev/v2/character';
 //Browser XMLHttpRequest, built in the browser and require more code.
 var xhr = new XMLHttpRequest();
@@ -34,7 +34,7 @@ xhr.onreadystatechange = function () {
 xhr.open('GET', requestUrl);
 xhr.send();
 
-// copy pasted from stack overflow lol: https://stackoverflow.com/questions/51506579/sending-authorization-token-bearer-through-javascript
+// copy-pasted from stack overflow lol: https://stackoverflow.com/questions/51506579/sending-authorization-token-bearer-through-javascript
 $.ajax({
     url: requestUrl,
     type: 'GET',
@@ -45,14 +45,3 @@ $.ajax({
         console.log(response);
     }
 })
-
-// old broken
-//var baseLotrURL = "https://the-one-api.dev/v2/";
-//var charactersURL = baseLotrURL + "character" + "&appid=" + lotrAPI;
-//async function pullLotrAPI() {
-//    const lotrResponse = await fetch(charactersURL);
-//    const charactersInfo = await lotrResponse.json();
-//    console.log(charactersInfo);
-//}
-//// not working????????????
-//pullLotrAPI();
