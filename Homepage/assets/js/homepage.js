6f00845589b4dbac4c4b965579e08c60f1d7e983
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-07 19:53:06 -0600

    its not working, presumably bad url. committing anyways to save.

--- Homepage/assets/js/homepage.js
+++ Homepage/assets/js/homepage.js
@@ -10,3 +10,17 @@ var lotrAPI = "ql0OUYOIpoNhyZBMrR2O";
 
 //TO DO: Create click event for search button on homepage
 //TO DO: Create click event for hamburger on mobile homepage
+
+
+
+
+// api stuffs
+var baseLotrURL = "https://the-one-api.dev/v2/";
+var charactersURL = baseLotrURL + "character" + "&appid=" + lotrAPI;
+async function pullLotrAPI() {
+    const lotrResponse = await fetch(charactersURL);
+    const charactersInfo = await lotrResponse.json();
+    console.log(charactersInfo);
+}
+// not working????????????
+pullLotrAPI();
