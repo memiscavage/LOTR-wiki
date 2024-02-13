42639bed03983b057157f9e0e563cc5c60c0a8ed
Mariel Miscavage <memiscavage@gmail.com>
2024-02-13 15:13:26 -0600

    linked the api to the infopages html and began a function to add character name

--- InfoPages/assets/infopage.js
+++ InfoPages/assets/infopage.js
@@ -1,4 +1,47 @@
 //TO DO: Declare variables for document.querySelector to dynamically change text through HTML when 
 //displaying info on content page
-//TO DO: add other variables here
 
+// API Keys
+// LOTR Key
+var lotrAPI = "ql0OUYOIpoNhyZBMrR2O";
+
+
+// API Pulls
+
+// LOTR API pull:
+// copy-pasted from 06-Server-Side-APIs > 01-Activities > 05-Ins_Different_APIs > assets > js > script.js
+var requestUrl = 'https://the-one-api.dev/v2/character';
+//Browser XMLHttpRequest, built in the browser and require more code.
+var lotrInfo = "";
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        console.log('XMLHttpRequest Response \n-------------');
+        console.log(xhr.response);
+    }
+};
+xhr.open('GET', requestUrl);
+xhr.send();
+// copy-pasted from stack overflow lol: https://stackoverflow.com/questions/51506579/sending-authorization-token-bearer-through-javascript
+$.ajax({
+    url: requestUrl,
+    type: 'GET',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer " + lotrAPI);
+    },
+    success: function (response) {
+        console.log(response);
+        lotrInfo = response;
+
+        // how to grab specific info:
+        console.log(lotrInfo["docs"]["40"]["name"]);
+    }
+
+})
+
+function getCharacterInfo(lotrInfo) {
+    const name = document.getElementById("charName")
+    name.innerText = lotrInfo["docs"]["40"]["name"]
+    console.log(name)
+    return name;
+}
